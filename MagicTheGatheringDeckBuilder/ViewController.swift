4a6bb7a0954d8c4593ee3aa5a0c452ae3e4f085e
alexis gauthier <alexisgauthier@alexiss-mac.local>
2018-08-29 07:07:23 -0700

    Initial Commit

---
+++ MagicTheGatheringDeckBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicTheGatheringDeckBuilder
+//
+//  Created by alexis gauthier on 29/08/18.
+//  Copyright © 2018 alexis gauthier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
